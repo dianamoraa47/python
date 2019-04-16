c88d5d1c41b46d802a902d40050f2da130331d44
dianamoraa47 <47768544+dianamoraa47@users.noreply.github.com>
2019-04-16 17:27:47 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+def add(a,b):
+	c = a+b
+	print(c)
+def sub(a,b):
+	c = a-b
+	print(c)
+def divide(a,b):
+    c = a/b
+    print(c)
+def multiply(a,b):
+    c = a*b
+    print(c)
+def modulus(a,b):
+    c = a%b
+    print(c)
+def whole_number_division(a,b):
+    c = a//b
+    print(c)
+def percentage(a,b):
+    c = a/b*100
+    print(c)
+
+ 
