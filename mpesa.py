1a86c1b9924732b3fb30ba9cb4952bb321e99d15
dianamoraa47 <47768544+dianamoraa47@users.noreply.github.com>
2019-04-23 16:14:54 +0300

    Add files via upload

---
+++ mpesa.py
@@ -0,0 +1,39 @@
+class Mpesa:
+
+    def __init__(self,number,name,balance,withdraw,deposit,min_balance):
+         self.number = number
+         self.name = name
+         self.balance = balance
+         self.min_balance = min_balance
+         self.withdraw = withdraw
+         self.deposit = deposit
+    
+     
+
+
+    def number(self):
+     	number = self.number
+     	return number    
+
+    
+    def name(self):
+    	name = self.name
+    	return name
+
+    def balance(self):
+    	balance = self.balance
+    	return balance
+    
+    def	min_balance(self):
+    	min_balance = self.min_balance
+    	return min_balance
+
+    def withdraw(self):
+    	withdraw
+    	return withdraw
+
+    def deposit(self):
+        deposit = self.deposit
+        return deposit
+
+
