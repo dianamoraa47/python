4e4fa88850e3849d7699a1e985e06571896e6747
dianamoraa47 <47768544+dianamoraa47@users.noreply.github.com>
2019-04-30 19:35:58 +0300

    Add files via upload

---
+++ Shapes.py
@@ -0,0 +1,68 @@
+class Circle:
+	def __init__(self,r):
+		 self.radius = r
+		 self.area = 0
+		 sel.circumference = 0
+
+    def area(self):
+        return self.radius**2*3.14
+        print(r)
+
+
+    def circumference (self):
+        return self.radius**2*22/7
+        print(r)
+
+class Square:
+	def __init__(self,a):
+         self.Square = a**2
+	def square(self):
+		return self.square 2**2
+    def perimeter(self):
+    	self.area = 4**2
+        return 4**2 self.square *3.14
+        print(a)
+
+class Rectangle:
+	def __init__(self,a):
+
+		self.rectangle = w*l
+		
+	def rectangle(self):
+	return 4*2 self.rectangle
+
+	def perimeter(self):
+		return self.rectangle 
+		self.area= 2(l+w)
+		print(w*l)
+
+class Sphere:
+	def __init__(self,r):
+
+    def sphere(self):
+		self.Sphere = r
+		self.area =  4 * 3.14 *(r*r)
+		return (self.area)
+		print(r)
+
+
+    def volume(self):
+	self.volume = v
+	self.volume = (4/3) * 3.14 * (r * r * r)
+        return (self.volume)
+        print(v)
+
+
+
+
+
+	
+		
+
+
+
+
+
+
+    
+    
