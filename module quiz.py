47f925c66526a655ee44f8c85b08a063bb0844dc
dianamoraa47 <47768544+dianamoraa47@users.noreply.github.com>
2019-05-07 19:20:16 +0300

    Add files via upload

---
+++ module quiz.py
@@ -0,0 +1,70 @@
+
+tens = ["1","2","3","4","5","6","7","8","9"]
+for m in tens:
+	print(m*10)
+	
+
+m = [2,4,5,6,7,8,10,12]
+  for item in m:
+	print(item*2)
+
+a = [9,1,4,7,3]
+b =	[5,2,6,8,0]
+
+b.append(a)
+print (a)
+
+range_sum = ["1","2","3","4""n"]
+for range_sum in x:
+print(sum())
+
+
+
+largest = [12,2,11,34,23,54,45]
+for largest in m:
+ print(max(m))
+
+
+ smallest = [5,78,9,45,3,0,7,2,]
+for smallest in w:
+print(min(w)
+
+
+my_modulus = {[10,20]}
+for my_modulus in r range(10) if 
+
+r%3
+
+
+
+
+
+students = [{'balance': 1000, 'name': 'Irene'}, {'balance': 2000, 'name': 'Pauline'}, {'balance': 3000, 'name': 'Naima'}, {'balance': 1000, 'name': 'Rose'}]
+students =dict({balance:1000, name:Irene,balance:2000,name:Pauline,balance:3000,name:Naima,balance:1000,name:Rose})
+students = dict(balance=1000,name=Irene,balance=2000,name=Pauline,balance=Naima,balance=Rose)
+
+
+name =[];
+balance =[];
+
+if(name="Irene",balance="3000"
+	print "hello{}, your balance is{}."
+	)
+
+if(name="Pauline",balance="2000"
+	print "hello{}, your balance is{}."
+	)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
